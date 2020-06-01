492041d9909e366616ad117ae18d517ca4d02c09
Tan Nguyen <tannguyen1024@gmail.com>
2020-05-31 21:48:50 -0500

    Updated edit story

--- src/components/Admin_Edit_Story/Admin_Edit_Story.jsx
+++ src/components/Admin_Edit_Story/Admin_Edit_Story.jsx
@@ -6,10 +6,14 @@ import styles from '../Style/Style'
 
 class Admin_Create_Story extends Component {
 
-    state = { story_id: this.props.editStory.story_title, story_title: '', story_description: '', story_path: '', user_id: this.props.user.id }
+    state = { story_id: '', story_title: '', story_description: '', story_path: '', user_id: this.props.user.id }
 
     componentDidMount = () => {
         this.props.dispatch({ type: 'GET_EDIT_STORY', payload: this.props.match.params.id });
+    }
+
+    handleChangeDescription = (event) => {
+        this.setState({story_description: event.target.value})
     }
 
     handleChangePath = (event) => {
@@ -17,12 +21,31 @@ class Admin_Create_Story extends Component {
     }
 
     handleClick = () => {
-        console.log('State is:', this.state)
+        this.setState({ story_id: this.props.editStory.id });
+        if (this.state.story_title==='') {
+            this.setState({ story_title: this.props.editStory.story_title }), () => {
+                console.log('Title was not changed.')
+            }
+        }
+        if (this.state.story_description==='') {
+            this.setState({ story_description: this.props.editStory.story_description }), () => {
+                console.log('Description was not changed.')
+            }
+            
+        }
+        if (this.state.story_path === '') {
+            this.setState({ story_path: this.props.editStory.story_path }), () => {
+                console.log('Path was not changed.')
+            }
+            
+        } 
+        console.log('State is now:',this.state)
     }
 
     render() {
         let story=this.props.editStory;
         const { classes } = this.props;
+        // console.log('editStory is currently------>',this.props.editStory)
         return (
             <>
                     <Card className={classes.snippet}>
@@ -43,7 +66,7 @@ class Admin_Create_Story extends Component {
                                     <label>Title: </label><Input multiline defaultValue={story.story_title} placeholder="Insert story title here." />
                                 </Typography>
                                 <Typography className={classes.cursive} variant="body2" color="textSecondary" component="div">
-                                <label>Description: </label><TextField color="secondary" defaultValue={story.story_description} multiline fullWidth={true} placeholder="Insert description here." />
+                                <label>Description: </label><TextField onChange={this.handleChangeDescription} color="secondary" defaultValue={story.story_description} multiline fullWidth={true} placeholder="Insert description here." />
                                 </Typography>
                             <Typography className={classes.cursive} variant="body2" color="textSecondary" component="div">
                                     <label>Image URL: </label><TextField onChange={this.handleChangePath} color="secondary" defaultValue={story.story_path} multiline fullWidth={true} placeholder="Insert path to image here.  Example: http://address.com/picture.png" />
